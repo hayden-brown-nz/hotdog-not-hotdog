22307cce1cb7a28c35a2256ca5eff422a4d2091e
Hayden Brown <hayden@thebrowns.net.nz>
2019-03-12 23:20:09 +1300

    Added app permissions check/requests. UI tweaks.

--- app/src/main/java/com/hbindustries/hotdognothotdog/MainActivity.java
+++ app/src/main/java/com/hbindustries/hotdognothotdog/MainActivity.java
@@ -1,15 +1,22 @@
 package com.hbindustries.hotdognothotdog;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+
 import androidx.exifinterface.media.ExifInterface;
+
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,12 +25,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -33,12 +38,19 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 200;
+
     static final int REQUEST_TAKE_PHOTO = 1;
     ImageView imageView;
     Button btnCamera;
     TextView categoryTextView;
     String currentPhotoPath;
     ImageClassifier imgClassifier;
+    Activity thisActivity;
+
+    public MainActivity() {
+        thisActivity = this;
+    }
 
     final String[] categories = {
             "Hotdog !",
@@ -62,6 +74,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    private boolean checkappPermissions() {
+
+        if (ContextCompat.checkSelfPermission(thisActivity, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            if (ActivityCompat.shouldShowRequestPermissionRationale(thisActivity, Manifest.permission.CAMERA)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                Toast.makeText(getApplicationContext(), "This app requires the the camera permission to operate.", Toast.LENGTH_LONG).show();
+                checkappPermissions();
+                return false;
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(thisActivity,
+                        new String[]{Manifest.permission.CAMERA},
+                        CAMERA_PERMISSION_REQUEST_CODE);
+                return false;
+            }
+        } else {
+            // Permission already granted
+            return true;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -74,35 +108,65 @@ public class MainActivity extends AppCompatActivity {
             ex.printStackTrace();
         }
 
-
         imageView = (ImageView) findViewById(R.id.imageView);
         btnCamera = (Button) findViewById(R.id.btnCamera);
         categoryTextView = (TextView) findViewById(R.id.categoryTextView);
 
+        // Show background image initially (Resize the bitmap and load it)
+        Bitmap bMap = BitmapFactory.decodeResource(getResources(), R.drawable.background_img);
+        Bitmap bMapScaled = Bitmap.createScaledBitmap(bMap, 400, 600, true);
+        imageView.setImageBitmap(bMapScaled);
+        imageView.setVisibility(View.VISIBLE);
+
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick (View view) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                // Ensure that there's an activity available to handle the intent
-                if (intent.resolveActivity(getPackageManager()) != null) {
-                    File photoFile = null;
-                    try {
-                        photoFile = createImageFile();
-                    } catch (IOException ex) {
-                        // Error occurred when creating file
-                        Log.e("HotdogNotHotdog", "Exception: " + ex.toString());
-                    }
-
-                    // Continue if file was created
-                    if (photoFile != null) {
-                        Uri photoUri = FileProvider.getUriForFile(getApplicationContext(), "com.example.android.fileprovider", photoFile);
-                        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
-                        startActivityForResult(intent, REQUEST_TAKE_PHOTO);
-                    }
+            public void onClick(View view) {
+                if (checkappPermissions()) {
+                    takePhoto();
                 }
-
             }
         });
+    }
+
+
+    private void takePhoto() {
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's an activity available to handle the intent
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred when creating file
+                Log.e("HotdogNotHotdog", "Exception: " + ex.toString());
+            }
+
+            // Continue if file was created
+            if (photoFile != null) {
+                Uri photoUri = FileProvider.getUriForFile(getApplicationContext(), "com.example.android.fileprovider", photoFile);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(intent, REQUEST_TAKE_PHOTO);
+            }
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case CAMERA_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission granted
+                    takePhoto();
+                } else {
+                    // Permission denied
+                    Toast.makeText(getApplicationContext(), "This app does not have permissions to use the camera. Please grant camera permissions to this app.", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            // Other 'case' lines to check for other requested permissions
+        }
     }
 
     @Override
@@ -110,8 +174,8 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         // Image View dimensions
-        final int viewWidth = imageView.getWidth()/2;
-        final int viewHeight = imageView.getHeight()/2;
+        final int viewWidth = imageView.getWidth() / 2;
+        final int viewHeight = imageView.getHeight() / 2;
 
         if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
 
@@ -141,11 +205,11 @@ public class MainActivity extends AppCompatActivity {
                 String imageType = bmOptions.outMimeType;
 
                 // Scale image to imageView
-                bmOptions.inSampleSize =  calculateInSampleSize(bmOptions, viewWidth, viewHeight);
+                bmOptions.inSampleSize = calculateInSampleSize(bmOptions, viewWidth, viewHeight);
                 bmOptions.inJustDecodeBounds = false;
                 Bitmap scaledBitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
 
-                switch(orientation) {
+                switch (orientation) {
                     case ExifInterface.ORIENTATION_ROTATE_90:
                         scaledBitmap = rotateImage(scaledBitmap, 90);
                         break;
